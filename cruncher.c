16431762dc011f7ab73e5d0e1da9fcd91ae41c72
Julia <juliaanten@hotmail.com>
2017-09-19 14:47:07 +0200

    added comments

--- cruncher.c
+++ cruncher.c
@@ -6,8 +6,6 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <time.h> 
-
 #include "utils.h"
 
 #define QUERY_FIELD_QID 0
@@ -21,34 +19,41 @@
 Person *person_map;
 unsigned int *knows_map;
 unsigned short *interest_map;
-
 unsigned long person_length, knows_length, interest_length;
-
 FILE *outfile;
 
+/**
+* Compares result and lines up the results.
+*/
 int result_comparator(const void *v1, const void *v2) {
-    Result *r1 = (Result *) v1;
-    Result *r2 = (Result *) v2;
-    if (r1->score > r2->score)
-        return -1;
-    else if (r1->score < r2->score)
-        return +1;
-    else if (r1->person_id < r2->person_id)
-        return -1;
-    else if (r1->person_id > r2->person_id)
-        return +1;
-     else if (r1->knows_id < r2->knows_id)
-        return -1;
-    else if (r1->knows_id > r2->knows_id)
-        return +1;
-    else
-        return 0;
+	Result *r1 = (Result *) v1;
+	Result *r2 = (Result *) v2;
+
+	if (r1->score > r2->score)
+		return -1;
+	else if (r1->score < r2->score)
+		return +1;
+	else if (r1->person_id < r2->person_id)
+		return -1;
+	else if (r1->person_id > r2->person_id)
+		return +1;
+	else if (r1->knows_id < r2->knows_id)
+		return -1;
+	else if (r1->knows_id > r2->knows_id)
+		return +1;
+	else
+		return 0;
 }
 
+
+/**
+* Calculates the interest score.
+*/
 unsigned char get_score(Person *person, unsigned short areltd[]) {
 	long interest_offset;
 	unsigned short interest;
 	unsigned char score = 0;
+
 	for (interest_offset = person->interests_first; 
 		interest_offset < person->interests_first + person->interest_n; 
 		interest_offset++) {
@@ -57,7 +62,7 @@ unsigned char get_score(Person *person, unsigned short areltd[]) {
 		if (areltd[0] == interest) score++;
 		if (areltd[1] == interest) score++;
 		if (areltd[2] == interest) score++;
-		// early exit
+		
 		if (score > 2) {
 			break;
 		}
@@ -65,6 +70,10 @@ unsigned char get_score(Person *person, unsigned short areltd[]) {
 	return score;
 }
 
+
+/**
+* Checks if person likes the artist.
+*/
 char likes_artist(Person *person, unsigned short artist) {
 	long interest_offset;
 	unsigned short interest;
@@ -83,105 +92,81 @@ char likes_artist(Person *person, unsigned short artist) {
 	return likesartist;
 }
 
+
+/**
+* Drives the search query.
+*/
 void query(unsigned short qid, unsigned short artist, unsigned short areltd[], unsigned short bdstart, unsigned short bdend) {
 	unsigned int person_offset;
-	unsigned long knows_offset, knows_offset2;
-
-	Person *person, *knows, *friend;
-	unsigned char score;
-	//char* p_fan = malloc(person_length/sizeof(Person));
-	char* p_score = malloc(person_length/sizeof(Person));
-
 	unsigned int result_length = 0, result_idx, result_set_size = 1000;
-	Result* results = malloc(result_set_size * sizeof (Result));
-	printf("Running query %d\n", qid);
+	unsigned long knows_offset, knows_offset2;
+	unsigned char score;
 
+	char* p_score = malloc(person_length/sizeof(Person));
+	Person *person, *knows, *friend;
+	Result* results = malloc(result_set_size * sizeof (Result));
+
+	// checks birthday, interest in artist and interest score
 	for (person_offset = 0; person_offset < person_length/sizeof(Person); person_offset++) {
-        p_score[person_offset] = 0;
-        //p_fan[person_offset] = 0;
+		p_score[person_offset] = 0;
 		person = &person_map[person_offset];
 
-		if (person_offset > 0 && person_offset % REPORTING_N == 0) {
-			printf("%.2f%%\n", 100 * (person_offset * 1.0/(person_length/sizeof(Person))));
-		}
-
-		// filter by birthday
 		if (person->birthday < bdstart || person->birthday > bdend) continue; 
 
-		// person must not like artist yet
 		if (likes_artist(person, artist))continue;
-		
-		// get the score from each person P
-		p_score[person_offset] = get_score(person, areltd);
 
+		p_score[person_offset] = get_score(person, areltd);
 	}
 
+	// runs through all the friends of person
 	for (person_offset = 0; person_offset < person_length/sizeof(Person); person_offset++) {
 
 		friend = &person_map[person_offset];
 
-		//if(p_fan[person_offset] == 0)continue;
-
-	// friend must already like the artist
+		// checks if persons friend likes the artist
 		if (!likes_artist(friend, artist))continue;
 
-		// check if friend lives in same city and likes artist 
+		// for friends who like the artist, make offset, checks score
 		for (knows_offset = friend->knows_first; 
 			knows_offset < friend->knows_first + friend->knows_n; 
 			knows_offset++) {
-	
-	     	// Consider a new offset for the friends
+
 			knows_offset2 = knows_map[knows_offset];
-	
-			// Set each Person F
+
 			person = &person_map[knows_offset2];
 
-			// Person and friend must live in same location
-			//if (person->location != friend->location) continue; 
-
-
-			// Check if friends score is sufficient
 			if(p_score[knows_offset2] < 1)continue;
 
-			
-			// friendship must be mutual
-			// for (knows_offset2 = knows->knows_first;
-			// 	knows_offset2 < knows->knows_first + knows->knows_n;
-			// 	knows_offset2++) {
-			
-				//if (knows_offset2 == person_offset) {
-					// realloc result array if we run out of space
-					if (result_length >= result_set_size) {
-						result_set_size *= 2;
-						results = realloc(results, result_set_size * sizeof (Result));
-					}
-					results[result_length].person_id = knows_offset2;
-					results[result_length].knows_id = person_offset;
-					results[result_length].score = p_score[knows_offset2];
-					result_length++;
-					//break;
-				//}
-			//}
+			// allocates new memory if memory is too small to save results
+			if (result_length >= result_set_size) {
+				result_set_size *= 2;
+				results = realloc(results, result_set_size * sizeof (Result));
+			}
+
+			results[result_length].person_id = knows_offset2;
+			results[result_length].knows_id = person_offset;
+			results[result_length].score = p_score[knows_offset2];
+			result_length++;
+		}
 	}
-}
 
-
-	// sort result
+	// sort results
 	qsort(results, result_length, sizeof(Result), &result_comparator);
 
-	// output
+	// write output
 	for (result_idx = 0; result_idx < result_length; result_idx++) {
 		person_offset = results[result_idx].person_id;
 		knows_offset = results[result_idx].knows_id;
 		person = &person_map[person_offset];
 		knows = &person_map[knows_offset];
 
+		// mutuality check
 		for (knows_offset2 = person->knows_first;
-		     knows_offset2 < person->knows_first + person->knows_n;
-		     knows_offset2++) {
-			
-			if(knows_map[knows_offset2] == knows_offset) {
+			knows_offset2 < person->knows_first + person->knows_n;
+			knows_offset2++) {
 
+			// if friendship is mutual, print to output file
+			if(knows_map[knows_offset2] == knows_offset) {
 				fprintf(outfile, "%d|%d|%lu|%lu\n", qid, results[result_idx].score, 
 				person->person_id, knows->person_id);
 			}
@@ -189,6 +174,10 @@ void query(unsigned short qid, unsigned short artist, unsigned short areltd[], u
 	}
 }
 
+
+/**
+* Makes up outline for results.
+*/
 void query_line_handler(unsigned char nfields, char** tokens) {
 	unsigned short q_id, q_artist, q_bdaystart, q_bdayend;
 	unsigned short q_relartists[3];
@@ -204,29 +193,26 @@ void query_line_handler(unsigned char nfields, char** tokens) {
 	query(q_id, q_artist, q_relartists, q_bdaystart, q_bdayend);
 }
 
+
 int main(int argc, char *argv[]) {
 	if (argc < 4) {
 		fprintf(stderr, "Usage: [datadir] [query file] [results file]\n");
 		exit(1);
 	}
-	/* memory-map files created by loader */
+
+	// memory-map files created by loader
 	person_map   = (Person *)         mmapr(makepath(argv[1], "person",   "bin"), &person_length);
 	interest_map = (unsigned short *) mmapr(makepath(argv[1], "interest", "bin"), &interest_length);
 	knows_map    = (unsigned int *)   mmapr(makepath(argv[1], "knows",    "bin"), &knows_length);
 
-  	outfile = fopen(argv[3], "w");  
-  	if (outfile == NULL) {
-  		fprintf(stderr, "Can't write to output file at %s\n", argv[3]);
+	outfile = fopen(argv[3], "w");  
+	if (outfile == NULL) {
+		fprintf(stderr, "Can't write to output file at %s\n", argv[3]);
 		exit(-1);
-  	}
+	}
 
-  	clock_t start = clock(), diff;
-
-	 /* run through queries */
+	// run through queries
 	parse_csv(argv[2], &query_line_handler);
-	diff = clock() - start;
 
-	int msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("Time taken %d seconds %d milliseconds", msec/1000, msec%1000);
 	return 0;
 }
